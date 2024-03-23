33ba6be39801931e83e75e8a33751b3d8c382e93
mrlptrc <murilopatricio@gmail.com>
2024-03-23 05:30:08 -0300

    feat: header + main section + search input.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration()
+    provideClientHydration(), provideAnimationsAsync()
   ]
 };
